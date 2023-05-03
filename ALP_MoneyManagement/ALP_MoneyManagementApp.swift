07788034d03426d5ce5e6534cb11795e09df1e72
gabrielabernice <gbernice@student.ciputra.ac.id>
2023-05-03 22:30:09 +0700

    Initial Commit

---
+++ ALP_MoneyManagement/ALP_MoneyManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  ALP_MoneyManagementApp.swift
+//  ALP_MoneyManagement
+//
+//  Created by MacBook Pro on 03/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ALP_MoneyManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
