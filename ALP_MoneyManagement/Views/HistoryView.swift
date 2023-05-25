da3d56823ff58d29057c7d5c32f1015ece350a3c
kathlynee <ksarahfania@student.ciputra.ac.id>
2023-05-25 12:46:36 +0700

    Make History View can show Input Income and Expenses data, and can be deleted

--- ALP_MoneyManagement/Views/HistoryView.swift
+++ ALP_MoneyManagement/Views/HistoryView.swift
@@ -8,21 +8,116 @@
 import SwiftUI
 
 struct HistoryView: View {
-//    @State var SavingsHistory: [Savings] = []
-    @State var history: [History] = []
-    
+    @State var incomeHistory: [History] = []
+    @State var expensesHistory: [History] = []
+
     var body: some View {
         VStack {
-            Text("History")
-                .font(.title)
-            ForEach(history, id: \.self) { expense in
-                Text("\(expense.name) - Rp. \(expense.amount) - \(expense.date, formatter: dateFormatter)")
+            List {
+                Section(header: Text("Income")) {
+                    ForEach(incomeHistory) { history in
+                        NavigationLink(destination: EditHistoryView(history: $incomeHistory[getIndex(for: history, in: incomeHistory)])) {
+                            HistoryRow(history: history)
+                        }
+                    }
+                    .onDelete { offsets in
+                        deleteHistory(at: offsets, type: "Income")
+                    }
+                }
+                
+                Section(header: Text("Expenses")) {
+                    ForEach(expensesHistory) { history in
+                        NavigationLink(destination: EditHistoryView(history: $expensesHistory[getIndex(for: history, in: expensesHistory)])) {
+                            HistoryRow(history: history)
+                        }
+                    }
+                    .onDelete { offsets in
+                        deleteHistory(at: offsets, type: "Expenses")
+                    }
+                }
             }
-            Text("This feature is still developing\nPlease wait for an App update in the future")
-                .multilineTextAlignment(.center)
-                .padding(.vertical)
+            .listStyle(GroupedListStyle())
         }
-        .bold()
+        .navigationTitle("History")
+        .onAppear {
+            loadDataFromUserDefaults()
+        }
+    }
+    
+    func deleteHistory(at offsets: IndexSet, type: String) {
+        if type == "Income" {
+            incomeHistory.remove(atOffsets: offsets)
+        } else if type == "Expenses" {
+            expensesHistory.remove(atOffsets: offsets)
+        }
+        
+        saveDataToUserDefaults()
+    }
+    
+    func saveDataToUserDefaults() {
+        let encoder = JSONEncoder()
+        
+        if let encodedIncomeData = try? encoder.encode(incomeHistory) {
+            UserDefaults.standard.set(encodedIncomeData, forKey: "incomeHistory")
+        }
+        
+        if let encodedExpensesData = try? encoder.encode(expensesHistory) {
+            UserDefaults.standard.set(encodedExpensesData, forKey: "expensesHistory")
+        }
+    }
+    
+    func loadDataFromUserDefaults() {
+        if let incomeData = UserDefaults.standard.data(forKey: "incomeHistory") {
+            let decoder = JSONDecoder()
+            if let decodedData = try? decoder.decode([History].self, from: incomeData) {
+                incomeHistory = decodedData
+            }
+        }
+        
+        if let expensesData = UserDefaults.standard.data(forKey: "expensesHistory") {
+            let decoder = JSONDecoder()
+            if let decodedData = try? decoder.decode([History].self, from: expensesData) {
+                expensesHistory = decodedData
+            }
+        }
+    }
+    
+    func getIndex(for history: History, in array: [History]) -> Int {
+        guard let index = array.firstIndex(where: { $0.id == history.id }) else {
+            fatalError("History not found")
+        }
+        return index
+    }
+}
+
+struct EditHistoryView: View {
+    @Binding var history: History
+    
+    var body: some View {
+        // Tampilan untuk mengedit entri History
+        VStack {
+            Text("Name: \(history.name)")
+            Text("Amount: \(history.amount)")
+            Text("Date: \(history.date, formatter: dateFormatter)")
+            // Tambahkan informasi lain yang ingin diubah
+        }
+    }
+}
+
+struct HistoryRow: View {
+    var history: History
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Name: \(history.name)")
+                .font(.headline)
+            Text("Amount: Rp. \(history.amount)")
+                .font(.subheadline)
+            Text("Date: \(history.date, formatter: dateFormatter)")
+                .font(.subheadline)
+            // Tambahkan informasi lain yang ingin ditampilkan
+        }
+        .padding()
     }
 }
 
@@ -33,9 +128,12 @@ private let dateFormatter: DateFormatter = {
     return formatter
 }()
 
+
 struct HistoryView_Previews: PreviewProvider {
-    static let modelData = ModelData()
-    
     static var previews: some View {
-        HistoryView()
+        HistoryView(incomeHistory: [
+            History(id: 1, name: "Income 1", amount: 50000, date: Date(), type: "Income"),
+            History(id: 2, name: "Income 2", amount: 75000, date: Date(), type: "Income")
+        ])
     }
+}
