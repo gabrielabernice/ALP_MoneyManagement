5ab68c910b9e89cd1f0f9a7a955d858d82362668
Vebrillia <vebrillias@gmail.com>
2023-05-26 20:54:42 +0700

    Fixing InputIncome view and creating error handling for nil selected category // Moving text and text field moving problem is solved

--- ALP_MoneyManagement/Views/InputIncome.swift
+++ ALP_MoneyManagement/Views/InputIncome.swift
@@ -25,89 +25,184 @@ struct InputIncome: View {
     
     @State var name : String = ""
     
+    @State var showFailMessage = false
     
-    @State private var failMessage: String = ""
-    @State private var showFailMessage: Bool = false
+    
     
     var body: some View {
-        //        NavigationView {
-        VStack {
-            Group {
-                Text("test")
-            }
-            Group {
-                Text("Date: \(date, formatter: dateFormatter)")
-                    .font(.title2)
-                
-                Button(action: {
-                    isExpanded.toggle()
-                }, label: {
-                    Text("Select a date")
-                        .padding(.bottom)
-                })
-                
-                if isExpanded {
-                    DatePicker(
-                        "",
-                        selection: $date,
-                        displayedComponents: [.date]
-                    )
-                    .datePickerStyle(WheelDatePickerStyle())
-                }
-                
-                Text("Source of Income : \n\(selectedOption?.incomeCategory ?? "")")
-                    .font(.title2)
-                    .multilineTextAlignment(.center)
-                
-                Menu {
-                    ForEach(income, id: \.self) { incomes in
-                        Button(action: {
-                            selectedOption = incomes
-                        }) {
-                            Text(incomes.incomeCategory)
+        GeometryReader { geometry in
+            VStack {
+                VStack(alignment: .leading, spacing: 30){
+                    Text("Input Income")
+                        .foregroundColor(.white)
+                        .font(.system(size: 32, weight: .bold))
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .padding(.top, -50)
+                        .offset(x: 12)
+                    
+                    VStack(spacing: -5) {
+                        Text("Date ")
+                            .foregroundColor(.white)
+                            .font(.system(size: 22, weight: .bold))
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundColor(Color.white)
+                            .frame(width: 350, height: 50)
+                            .padding()
+                            .overlay(
+                                HStack{
+                                    Text("\(date, formatter: dateFormatter)")
+                                    
+                                    
+                                    Spacer().frame(width: 85)
+                                    
+                                    Button(action: {
+                                        isExpanded.toggle()
+                                    }, label: {
+                                        Text("Select a date")
+                                            .padding()
+                                        
+                                    })
+                                    
+                                }
+                            )
+                        
+                        if isExpanded {
+                            DatePicker(
+                                "",
+                                selection: $date,
+                                displayedComponents: [.date]
+                            )
+                            .datePickerStyle(WheelDatePickerStyle())
                         }
                     }
-                } label: {
-                    Label("Select an option", systemImage: "arrowtriangle.down.fill")
-                        .foregroundColor(Color(red: 0.4, green: 0.6, blue: 1.0))
-                        .padding(.bottom)
-                }
-                
-                Text("Amount : ")
-                    .font(.title2)
-                
-                HStack{
-                    Text("Rp. ")
-                        .font(.title2)
+                    .padding(.bottom, -20)
                     
-                    TextField("ex : 50000", text: $amount)
-                        .padding(.horizontal, 20)
-                        .padding(.vertical, 10)
-                        .font(.system(size: 16, weight: .bold))
-                        .background(Color.gray.opacity(0.1))
-                        .cornerRadius(10)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 10)
-                                .stroke(Color.black.opacity(0.3), lineWidth: 2)
-                        )
-                        .keyboardType(.numberPad)
+                    VStack(spacing: -5){
+                        Text("Source of Income")
+                            .foregroundColor(.white)
+                            .font(.system(size: 22, weight: .bold))
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundColor(Color.white)
+                            .frame(width: 350, height: 50)
+                            .padding()
+                            .overlay(
+                                HStack{
+                                    Text("\(selectedOption?.incomeCategory ?? "")")
+                                    Spacer().frame(width: 85)
+                                    
+                                    Menu {
+                                        ForEach(income, id: \.self) { incomes in
+                                            Button(action: {
+                                                selectedOption = incomes
+                                            }) {
+                                                Text(incomes.incomeCategory)
+                                            }
+                                        }
+                                    } label: {
+                                        Label("Select an option", systemImage: "arrowtriangle.down.fill")
+                                            .foregroundColor(Color(red: 0.4, green: 0.6, blue: 1.0))
+                                        
+                                    }
+                                    
+                                }
+                            )
+                        
+                    }
+                    .padding(.bottom, -33)
+                    
+                    VStack(spacing: -8){
+                        
+                        Text("Amount")
+                            .foregroundColor(.white)
+                            .font(.system(size: 22, weight: .bold))
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .offset(x:-12)
+                        
+                        TextField("ex : 50000", text: $amount)
+                            .padding()
+                            .background(Color(.white))
+                            .cornerRadius(10)
+                            .frame(width: 350, height: 90)
+                            .font(.system(size: 16, weight: .bold))
+                            .cornerRadius(10)
+                            .keyboardType(.numberPad)
+                        
+                        Text("*Only numbers above 0")
+                            .font(.system(size: 20))
+                            .foregroundColor(.red)
+                            .font(.caption)
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                            .font(.title)
+                            .opacity(!check ? 1 : 0)
+                        
+                        
+                    }
+                    .padding()
+                    .padding(.bottom, -50)
+                    
+                    VStack(spacing: -5){
+                        
+                        Text("Note ")
+                            .foregroundColor(.white)
+                            .font(.system(size: 22, weight: .bold))
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .offset(x:-12)
+                        
+                        TextField("Mcd", text: $name)
+                            .padding()
+                            .background(Color(.white))
+                            .cornerRadius(10)
+                            .frame(width: 350, height: 90)
+                            .font(.system(size: 16, weight: .bold))
+                            .cornerRadius(10)
+                            .keyboardType(.numberPad)
+                        
+                    }
+                    .padding()
+                    
+                    
+                    
+                    
+                    //batas vstack 1 untuk form
                 }
+                .padding(.top, 80)
+                .padding(.horizontal, 40)
+                .frame(width: geometry.size.width, height: geometry.size.height * 0.75)
+                .background(Color(hex: 0x6DA3FF))
+                .clipShape(BottomRoundedRectangle(radius:55))
+                .shadow(color: Color.black.opacity(0.3), radius: 18, x: 0, y: 5)
+                //                .onChange
                 
-                if !check{
-                    Text("Please enter a valid amount\n(only numbers above 0)")
-                        .foregroundColor(.red)
-                        .font(.caption)
-                        .multilineTextAlignment(.center)
+//                HStack{}.frame(height: -20)
+                
+                
+                    Text("Data successfully saved!")
                         .padding()
-                }
+                        .multilineTextAlignment(.center)
+                        .offset(y: 10)
+                        .opacity(appendIncome == true ? 1.0 : 0.0)
+                        .opacity(showFailMessage == false ? 1:0)
                 
+              
+                    Text("Please select an option")
+                        .multilineTextAlignment(.center)
+                        .offset(y: -40)
+                        .opacity(appendIncome == false ? 1:0)
+                        .opacity(showFailMessage == true ? 1.0 : 0.0)
+                        
                 
                 
                 Button("Save") {
                     if selectedOption == nil {
                         // Tampilkan pesan kesalahan karena opsi belum dipilih
-                        failMessage = "Please select an option"
+//                        failMessage = "Please select an option"
                         showFailMessage = true
+                        appendIncome = false
                     } else {
                         if check {
                             incomeHistory.append(History(id: index, category: selectedOption?.incomeCategory ?? "", amount: Int(amount) ?? 0, date: date, type: type, name: name))
@@ -118,52 +213,80 @@ struct InputIncome: View {
                             }
                             
                             appendIncome = true
+                            showFailMessage = false
                             index += 1
                         }
                     }
                 }
-                .fontWeight(.bold)
-                .foregroundColor(.white)
                 .padding()
-                .background(Color(red: 0.4, green: 0.6, blue: 1.0))
-                .cornerRadius(10)
+                .frame(width: geometry.size.width * 0.9)
+                .font(.system(size: 22, weight: .bold))
+                .foregroundColor(.white)
+                .background(Color(hex: 0x6DA3FF))
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .fontWeight(.bold)
+                .padding(.bottom, 60)
+                .offset(y: -10)
                 .disabled(!check)
                 
                 
-                if appendIncome{
-                    Text("Data successfully saved!")
-                        .padding()
-                        .multilineTextAlignment(.center)
-                }else if showFailMessage {
-                    Text(failMessage)
-                        .padding()
-                        .multilineTextAlignment(.center)
-                }
+                
+                
             }
-            
-            
-            
-        }
-        .onAppear {
-            if let data = UserDefaults.standard.data(forKey: "incomeHistory") {
-                if let decodedData = try? JSONDecoder().decode([History].self, from: data) {
-                    incomeHistory = decodedData
-                    index = incomeHistory.count
+            .onAppear {
+                if let data = UserDefaults.standard.data(forKey: "incomeHistory") {
+                    if let decodedData = try? JSONDecoder().decode([History].self, from: data) {
+                        incomeHistory = decodedData
+                        index = incomeHistory.count
+                    }
                 }
+                
+                let url = Bundle.main.url(forResource: "incomeData", withExtension: "json")!
+                let jsonData = try! Data(contentsOf: url)
+                let decoder = JSONDecoder()
+                income = try! decoder.decode([Income].self, from: jsonData)
             }
-            
-            let url = Bundle.main.url(forResource: "incomeData", withExtension: "json")!
-            let jsonData = try! Data(contentsOf: url)
-            let decoder = JSONDecoder()
-            income = try! decoder.decode([Income].self, from: jsonData)
         }
-        //        }
         .onChange(of: amount) { newValue in
             check = ((Int(newValue) ?? 0) >= 1)
         }
-        .padding(.horizontal, 30)
-        .bold()
+        .ignoresSafeArea(.all)
+    
     }
+    
+    struct BottomRoundedRectangle: Shape {
+        var radius: CGFloat
+        
+        func path(in rect: CGRect) -> Path {
+            var path = Path()
+            
+            let width = rect.size.width
+            let height = rect.size.height
+            
+            // Determine the corner radius
+            let cornerRadius = min(min(radius, height/2), width/2)
+            
+            // Create a rectangle with the bottom corners rounded
+            path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY - cornerRadius))
+            path.addArc(center: CGPoint(x: rect.maxX - cornerRadius, y: rect.maxY - cornerRadius),
+                        radius: cornerRadius,
+                        startAngle: Angle(degrees: 0),
+                        endAngle: Angle(degrees: 90),
+                        clockwise: false)
+            path.addLine(to: CGPoint(x: rect.minX + cornerRadius, y: rect.maxY))
+            path.addArc(center: CGPoint(x: rect.minX + cornerRadius, y: rect.maxY - cornerRadius),
+                        radius: cornerRadius,
+                        startAngle: Angle(degrees: 90),
+                        endAngle: Angle(degrees: 180),
+                        clockwise: false)
+            path.closeSubpath()
+            
+            return path
+        }
+    }
+    
 }
 
 private let dateFormatter: DateFormatter = {
